56b80e014b6569e9c749aa3bda28452c8c40624f
Nolan Byrnes <nolanehs@gmail.com>
2018-03-15 11:58:48 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,9 +5,9 @@ end
 
 def create
 if params[:name] = nil
-  redirect_to '/sessions/new'
+  redirect_to root_path
 elsif session[:name] = params[:name]
-redirect_to '/'
+redirect_to sessions_new_path
 end
 end
 def destroy
